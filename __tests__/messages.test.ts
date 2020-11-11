5d422872025cd98188390d4706c5dbea216f81b2
Yasir Munas <ymu@tiqri.com>
2020-11-11 18:47:50 +0530

    Updated test cased based on language

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,13 +1,21 @@
-import { norwegian } from "../src/common/error-configuration-data";
+import { english, errorType, norwegian } from "../src/common/error-configuration-data";
 import { showError } from "../src/message";
 
 
 describe("messages", () => {
-  it.todo("shows a message in english");
+  
 
-  it.todo("shows a message in another language");
+  it("shows a message in norwegian", () => {
+    expect(
+      showError(norwegian, { error: errorType.illegalArgument, key: "foo", value: "bar", errorMessage: '' })
+    ).toEqual("Verdien for 'foo' er ikke tillat: bar");
+  });
+ 
 
-  expect(
-    showError(norwegian, { error: "illegalArgument", key: "foo", value: "bar", errorMessage: '' })
-  ).toEqual("Verdien for 'foo' er ikke tillat: bar");
+  it("shows a message in english", () => {
+    expect(
+      showError(english, { error: errorType.ServerError, key: "", value: "", errorMessage: 'Typescript error tested' })
+    ).toEqual("The server returned an error: Typescript error tested");
+  });
+  
 });
