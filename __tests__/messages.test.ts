726c4fc686dee633945088a6a71bdc23f218e9e2
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-11-12 11:37:42 +0100

    messages should show a message in english (failing test)

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,5 +1,15 @@
+function showError(language: any, error: any) {
+  return "";
+}
+
+const english = {};
+
 describe("messages", () => {
-  it.todo("shows a message in english");
+  it("shows a message in english", () => {
+    expect(showError(english, { code: "generalError" })).toBe(
+      "Something went wrong"
+    );
+  });
 
   it.todo("shows a message in another language");
 });
