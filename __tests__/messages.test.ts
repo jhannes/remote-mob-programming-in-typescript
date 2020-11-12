167efce72fe7a7e29f34aad39f0b02cc29160649
HarishPradeep <49173580+HarishPradeep@users.noreply.github.com>
2020-11-12 17:37:20 +0530

    it shows error with multiple arguments

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,18 +1,42 @@
-interface ErrorMessages{
-   code:string;
-   errorMessage?:string;
+interface GeneralError {
+  code: "generalError";
 }
-function showError(language: any, error: ErrorMessages) {
-  if (error.code == "serverError") {
-    const {code,  errorMessage} = error;
-    return "Server return an error : " + errorMessage;
+
+interface ServerError {
+  code: "serverError";
+  errorMessage: string;
+}
+
+type ErrorMessages = GeneralError | ServerError | IllegalArgument;
+
+interface ApplicationLanguage {
+  generalError: string;
+  serverError: (errorMessage: string) => string;
+}
+
+interface IllegalArgument {
+  code: "illegalArgument";
+  key: string;
+  value: string;
+}
+
+function showError(language: ApplicationLanguage, error: ErrorMessages) {
+  if (error.code === "serverError") {
+    const { code, errorMessage } = error;
+    return language.serverError(errorMessage);
   } else {
-    return "Something went wrong";
+    return language.generalError;
   }
 }
 
-const english = {};
-const norwegian = {};
+const english: ApplicationLanguage = {
+  generalError: "Something went wrong",
+  serverError: (errorMessage) => "Server return an error " + errorMessage,
+};
+const norwegian: ApplicationLanguage = {
+  generalError: "Noe gikk galt",
+  serverError: (errorMessage) => "Server return an error " + errorMessage,
+};
 
 describe("messages", () => {
   it("shows a message in english", () => {
@@ -33,5 +57,15 @@ describe("messages", () => {
     expect(showError(norwegian, { code: "generalError" })).toBe(
       "Noe gikk galt"
     );
+  });
+
+  it("shows a message with multiple arguments", () => {
+    expect(
+      showError(norwegian, {
+        code: "illegalArgument",
+        key: "foo",
+        value: "bar",
+      })
+    ).toEqual("Verdien for 'foo' er ikke tillat: bar");
   });
 });
