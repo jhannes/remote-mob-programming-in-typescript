146889035267f7a0649dacd11597f2c8cbcabff1
Tharindu Gunawardhana <tgu@tiqri.com>
2020-11-17 20:03:26 +0530

    Refactor using keyof.

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,27 +1,27 @@
 const english: ApplicationLanguage = {
-  generalError: "Something went wrong",
-  serverError: (message: string) => "The server returned an error: " + message,
-  illegalArgument: (property: string, value: string) =>
+  generalError: () => "Something went wrong",
+  serverError: ({ message }) => "The server returned an error: " + message,
+  illegalArgument: ({ property, value }) =>
     `The value is illegal for ${property}: ${value}`,
 };
 const norwegian: ApplicationLanguage = {
-  generalError: "Noe gikk galt",
-  serverError: (message: string) => "erveren returnerte en feil: " + message,
-  illegalArgument: (property: string, value: string) =>
+  generalError: () => "Noe gikk galt",
+  serverError: ({ message }) => "erveren returnerte en feil: " + message,
+  illegalArgument: ({ property, value }) =>
     `Verdien for ${property} er ikke tillatt: ${value}`,
 };
 
 interface GeneralError {
-  error: "generalError";
+  error: keyof ApplicationLanguage;
 }
 
 interface ServerError {
-  error: "serverError";
+  error: keyof ApplicationLanguage;
   message: string;
 }
 
 interface IllegalArgument {
-  error: "illegalArgument";
+  error: keyof ApplicationLanguage;
   property: string;
   value: string;
 }
@@ -29,39 +29,41 @@ interface IllegalArgument {
 type ErrorMessages = GeneralError | ServerError | IllegalArgument;
 
 interface ApplicationLanguage {
-  generalError: string;
-  serverError: (message: string) => string;
-  illegalArgument: (property: string, value: string) => string;
+  generalError: (error: GeneralError) => string;
+  serverError: (error: ServerError) => string;
+  illegalArgument: (error: IllegalArgument) => string;
 }
 
 function showError(
   language: ApplicationLanguage,
   errorMessage: ErrorMessages
 ): string {
-  if (errorMessage.error === "generalError") {
-    return language.generalError;
-  } else if (errorMessage.error === "serverError") {
-    const { message } = errorMessage;
-    return language.serverError(message);
-  }
-  const { property, value } = errorMessage;
-  return language.illegalArgument(property, value);
+  return language[errorMessage.error](errorMessage as any);
 }
+
 describe("messages", () => {
   it("shows a message in english", () => {
-    expect(showError(english, { error: "generalError" })).toEqual(
-      "Something went wrong"
-    );
+    expect(
+      showError(english, {
+        error: "generalError",
+      })
+    ).toEqual("Something went wrong");
   });
   it("shows a message in english with a parameter", () => {
     expect(
-      showError(english, { error: "serverError", message: "Database timeout" })
+      showError(english, {
+        error: "serverError",
+        message: "Database timeout",
+      })
     ).toEqual("The server returned an error: Database timeout");
   });
+
   it("shows a message in norwegian", () => {
-    expect(showError(norwegian, { error: "generalError" })).toEqual(
-      "Noe gikk galt"
-    );
+    expect(
+      showError(norwegian, {
+        error: "generalError",
+      })
+    ).toEqual("Noe gikk galt");
   });
 
   it("shows a message in norwegian with two mparameters", () => {
