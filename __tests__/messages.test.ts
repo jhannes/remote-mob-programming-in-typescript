38211f570182a021bc6c3235e155e5390b019ffe
SL\msa <msa@tiqri.com>
2020-11-17 12:16:08 +0300

    shoes a message in english with parameter

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,20 +1,30 @@
-const english ={};
-function showError(language : any, error:any):string
-{
+const english = {};
+const norwegian = {};
+
+interface ErrorMessage{
+  error : string;
+  message? : string;
+}
+
+function showError(language: any, errorMessage: ErrorMessage): string {
+  const { error, message } = errorMessage;
+  if (error === "serverError") {
+    return `The server returned an error: ${message}`;
+  }
   return "Something went wrong";
 }
 describe("messages", () => {
-  it("shows a message in english",()=>{
-    expect(
-      showError(english, { error: "generalError"})
-    ).toEqual("Something went wrong");
-    
+  it("shows a message in english", () => {
+    expect(showError(english, { error: "generalError" })).toEqual(
+      "Something went wrong"
+    );
   });
-  it("shows a message in english with a parameter",()=>{
+  it("shows a message in english with a parameter", () => {
     expect(
-      showError(english, { error: "serverError", message:"Database timeout"})
+      showError(english, { error: "serverError", message: "Database timeout" })
     ).toEqual("The server returned an error: Database timeout");
-    
   });
-
+  it("shows a message in norwegian",() =>{
+    expect(showError(norwegian,{error :"generalError"})).toEqual("Noe gikk galt");
+  })
 });
