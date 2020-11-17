2cddb014d3f8ed000a2a828eda9fea288c2aec31
Harish Pradeep <npr@iverdi.no>
2020-11-17 15:05:11 +0530

    it shows a message in norwegian

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,17 +1,42 @@
-const english = {};
-const norwegian = {};
+const english: ApplicationLanguage = {
+  generalError: "Something went wrong",
+};
+const norwegian: ApplicationLanguage = {
+  generalError: "Noe gikk galt",
+};
 
-interface ErrorMessage{
-  error : string;
-  message? : string;
+interface GeneralError {
+  error: "generalError";
 }
 
-function showError(language: any, errorMessage: ErrorMessage): string {
-  const { error, message } = errorMessage;
-  if (error === "serverError") {
+interface ServerError {
+  error: "serverError";
+  message?: string;
+}
+
+interface IllegalArgument {
+  error: "illegalArgument";
+  property: string;
+  value: string;
+}
+
+type ErrorMessages = GeneralError | ServerError | IllegalArgument;
+
+interface ApplicationLanguage {
+  generalError: string;
+}
+
+function showError(
+  language: ApplicationLanguage,
+  errorMessage: ErrorMessages
+): string {
+  if (errorMessage.error === "generalError") {
+    return language.generalError;
+  } else if (errorMessage.error === "serverError") {
+    const { message } = errorMessage;
     return `The server returned an error: ${message}`;
   }
-  return "Something went wrong";
+  return "";
 }
 describe("messages", () => {
   it("shows a message in english", () => {
@@ -24,7 +49,19 @@ describe("messages", () => {
       showError(english, { error: "serverError", message: "Database timeout" })
     ).toEqual("The server returned an error: Database timeout");
   });
-  it("shows a message in norwegian",() =>{
-    expect(showError(norwegian,{error :"generalError"})).toEqual("Noe gikk galt");
-  })
+  it("shows a message in norwegian", () => {
+    expect(showError(norwegian, { error: "generalError" })).toEqual(
+      "Noe gikk galt"
+    );
+  });
+
+  it("shows a message in norwegian with two mparameters", () => {
+    expect(
+      showError(norwegian, {
+        error: "illegalArgument",
+        property: "foo",
+        value: "hoo",
+      })
+    ).toEqual("Noe gikk galt");
+  });
 });
