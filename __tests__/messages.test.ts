1e8f00273bf272bc793c60ca0af371c846c6b1dd
Mihisara Angulgamuwa <man@tiqri.com>
2020-11-17 11:57:46 +0300

    Show an error message in English

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,5 +1,20 @@
+const english ={};
+function showError(language : any, error:any):string
+{
+  return "Something went wrong";
+}
 describe("messages", () => {
-  it.todo("shows a message in english");
+  it("shows a message in english",()=>{
+    expect(
+      showError(english, { error: "generalError"})
+    ).toEqual("Something went wrong");
+    
+  });
+  it("shows a message in english with a parameter",()=>{
+    expect(
+      showError(english, { error: "serverError", message:"Database timeout"})
+    ).toEqual("The server returned an error: Database timeout");
+    
+  });
 
-  it.todo("shows a message in another language");
 });
