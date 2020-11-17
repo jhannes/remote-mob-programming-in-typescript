8ac67ebdf96d1a07daa0a5130251014720906b64
Mihisara Angulgamuwa <man@tiqri.com>
2020-11-17 10:52:31 +0300

    Using interpolation as it is and format the code with pritier

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -15,69 +15,74 @@ interface IIllegalArgument {
 type IError = IGeneralError | IServerError | IIllegalArgument;
 
 //Different error message templates for each type of error in different languages
-interface IMessageTemplate{
+interface IMessageTemplate {
   generalError: string;
   serverError: (errorMessage: string) => string;
   illegalArgument: (key: string, value: string) => string;
 }
 const english: IMessageTemplate = {
   generalError: "Something went wrong",
-  serverError: (errorMessage) => "The server returned an error: " + errorMessage,
-  illegalArgument: (key, value) => "The value is illegal for " + key + " : " + value,
-}; 
+  serverError: (errorMessage) =>
+    `The server returned an error: ${errorMessage}`,
+  illegalArgument: (key, value) => `The value is illegal for ${key} : ${value}`,
+};
 const norwegian: IMessageTemplate = {
   generalError: "Noe gikk galt",
-  serverError: (errorMessage) => "Serveren returnerte en feil: " + errorMessage,
-  illegalArgument: (key, value) => "Verdien for '" + key + "' er ikke tillat: " + value,
+  serverError: (errorMessage) => `Serveren returnerte en feil: ${errorMessage}`,
+  illegalArgument: (key, value) =>
+    `Verdien for '${key}' er ikke tillat: ${value}`,
 };
 
 //use relavant template based on the error type and language and return error message
-function showError(messageTemplate:IMessageTemplate, error:IError):string
-{
-  switch(error.code){
-  case "GeneralError":
-    return messageTemplate.generalError;
-  case "ServerError":
-    return messageTemplate.serverError(error.errorMessage);
-  case "IllegalArgument":
-    return messageTemplate.illegalArgument(error.key,error.value);
+function showError(messageTemplate: IMessageTemplate, error: IError): string {
+  switch (error.code) {
+    case "GeneralError":
+      return messageTemplate.generalError;
+    case "ServerError":
+      return messageTemplate.serverError(error.errorMessage);
+    case "IllegalArgument":
+      return messageTemplate.illegalArgument(error.key, error.value);
   }
 }
 describe("Messages", () => {
-  it("Shows a message in english", ()=>{
+  it("Shows a message in english", () => {
+    expect(showError(english, { code: "GeneralError" })).toEqual(
+      "Something went wrong"
+    );
+  });
+  it("Shows a message with an argument in english", () => {
     expect(
-      showError(english,{code:"GeneralError"})
-    ).toEqual("Something went wrong");
-  }  
-  );
-  it("shows a message with an argument in english", ()=>{
-    expect(
-      showError(english,{code:"ServerError",errorMessage:"Database timeout"})
+      showError(english, {
+        code: "ServerError",
+        errorMessage: "Database timeout",
+      })
     ).toEqual("The server returned an error: Database timeout");
-  }  
-  );
-  it("Shows a formatted message witn multiple arguments in english", ()=>{
+  });
+  it("Shows a formatted message witn multiple arguments in english", () => {
     expect(
-      showError(english,{code:"IllegalArgument",key:"foo",value:"bar"})
+      showError(english, { code: "IllegalArgument", key: "foo", value: "bar" })
     ).toEqual("The value is illegal for foo : bar");
-  }  
-  );
-  it("Shows a message in Norwegian", ()=>{
+  });
+  it("Shows a message in Norwegian", () => {
+    expect(showError(norwegian, { code: "GeneralError" })).toEqual(
+      "Noe gikk galt"
+    );
+  });
+  it("Shows a message with an argument in Norwegian", () => {
     expect(
-      showError(norwegian,{code:"GeneralError"})
-    ).toEqual("Noe gikk galt");
-  }  
-  );
-  it("shows a message with an argument in Norwegian", ()=>{
-    expect(
-      showError(norwegian,{code:"ServerError",errorMessage:"Database-tidsavbrudd"})
+      showError(norwegian, {
+        code: "ServerError",
+        errorMessage: "Database-tidsavbrudd",
+      })
     ).toEqual("Serveren returnerte en feil: Database-tidsavbrudd");
-  }  
-  );
-  it("Shows a formatted message witn multiple arguments in Norwegian", ()=>{
+  });
+  it("Shows a formatted message witn multiple arguments in Norwegian", () => {
     expect(
-      showError(norwegian,{code:"IllegalArgument",key:"foo",value:"bar"})
+      showError(norwegian, {
+        code: "IllegalArgument",
+        key: "foo",
+        value: "bar",
+      })
     ).toEqual("Verdien for 'foo' er ikke tillat: bar");
-  }  
-  );
+  });
 });
