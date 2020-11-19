fd28d4ab2079106eb8ddcde2ced7d01c71c463b9
SL\msa <msa@tiqri.com>
2020-11-19 14:19:31 +0300

    messges shows a message with two parameters

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,13 +1,21 @@
-
-function showMessage(language, message) {
+interface Message {
+  error: string;
+}
+function showMessage(language: any, message: Message) {
+  return "Something went wrong";
 }
 
 const english = {};
 
 describe("messages", () => {
   it("shows a message in english", () => {
-    expect(showMessage(english, { error: "generalError" }))
-      .toBe("Something went wrong");
+    expect(showMessage(english, { error: "generalError" })).toBe(
+      "Something went wrong"
+    );
   });
-
+  it("shows a meessage in english with two parameters",() =>{
+    expect(showMessage(english,{error : "serverError",errorMessage :"no database connection"})).toBe(
+      "Server return an error : no database connection"
+    )
+  })
 });
