5b8d8e47e16a483604f654fc72632bfd1371dde2
Mihisara Angulgamuwa <man@tiqri.com>
2020-11-19 14:31:13 +0300

    Shows a message with two parameters(to implement to show a message in norwegian)

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,11 +1,18 @@
 interface Message {
-  error: string;
+  error: "generalError" | "serverError";
+  errorMessage?: string;
 }
+
 function showMessage(language: any, message: Message) {
-  return "Something went wrong";
+  if (message.error == "generalError") {
+    return "Something went wrong";
+  } else if (message.error == "serverError") {
+    return "Server return an error : " + message.errorMessage;
+  }
 }
 
 const english = {};
+const norwegian = {};
 
 describe("messages", () => {
   it("shows a message in english", () => {
@@ -13,9 +20,17 @@ describe("messages", () => {
       "Something went wrong"
     );
   });
-  it("shows a meessage in english with two parameters",() =>{
-    expect(showMessage(english,{error : "serverError",errorMessage :"no database connection"})).toBe(
-      "Server return an error : no database connection"
-    )
-  })
+  it("shows a meessage in english with two parameters", () => {
+    expect(
+      showMessage(english, {
+        error: "serverError",
+        errorMessage: "no database connection",
+      })
+    ).toBe("Server return an error : no database connection");
+  });
+  it("shows a message in Norwegian", () => {
+    expect(showMessage(norwegian, { error: "generalError" })).toBe(
+      "Noe gikk galt"
+    );
+  });
 });
