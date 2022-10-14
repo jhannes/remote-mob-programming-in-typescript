c951c122639647334abfe4da778206105eb4ff51
Johannes Brodwall <johannes@brodwall.com>
2022-10-14 19:10:26 +0200

    generalError, networkError, illegalWeekday and illegalEmailDomain (partial)

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,5 +1,96 @@
-describe("messages", () => {
-  it.todo("shows a message in english");
+const LocalValues = ["en", "no"] as const;
 
-  it.todo("shows a message in another language");
+type Locale = typeof LocalValues[number];
+
+type Message =
+  | { code: "generalError" | "networkError" }
+  | { code: "illegalWeekday"; weekday: string }
+  | { code: "illegalEmailDomain"; email: string; allowedDomains: string[] };
+
+type LanguageStrings = {
+  generalError: string;
+  networkError: string;
+  illegalWeekday(arg: { weekday: string }): string;
+  illegalEmailDomain(arg: { email: string; allowedDomains: string[] }): string;
+};
+type LanguageBundle = Record<Locale, LanguageStrings>;
+
+const languages: LanguageBundle = {
+  no: {
+    generalError: "En feil har inntruffet",
+    networkError: "En nettverksfeil har inntruffet",
+    illegalWeekday: ({ weekday }) => "",
+    illegalEmailDomain: () => "",
+  },
+  en: {
+    generalError: "An error has occurred",
+    networkError: "",
+    illegalWeekday: ({ weekday }) => `'${weekday}' is not a day of the week`,
+    illegalEmailDomain: ({ email, allowedDomains }) =>
+      `Email must be in ${allowedDomains.join(" or ")}, was: ${email}`,
+  },
+};
+
+function localizedMessage(message: Message, locale: Locale): string {
+  if (message.code === "illegalWeekday") {
+    return languages[locale][message.code](message);
+  }
+  if (message.code === "illegalEmailDomain") {
+    return languages[locale][message.code](message);
+  }
+  return languages[locale][message.code];
+}
+
+describe("messages", () => {
+  it("shows a message in english", () => {
+    expect(localizedMessage({ code: "generalError" }, "en")).toBe(
+      "An error has occurred"
+    );
+  });
+
+  it("shows a message in norwegian", () => {
+    expect(localizedMessage({ code: "generalError" }, "no")).toBe(
+      "En feil har inntruffet"
+    );
+  });
+
+  it("shows other messages", () => {
+    expect(localizedMessage({ code: "networkError" }, "no")).toBe(
+      "En nettverksfeil har inntruffet"
+    );
+  });
+
+  it("shows message with argument", () => {
+    expect(
+      localizedMessage({ code: "illegalWeekday", weekday: "Payday" }, "en")
+    ).toBe("'Payday' is not a day of the week");
+  });
+
+  it("shows message with array argument", () => {
+    expect(
+      localizedMessage(
+        {
+          code: "illegalEmailDomain",
+          email: "bar@example.net",
+          allowedDomains: ["example.org", "example.com"],
+        },
+        "en"
+      )
+    ).toBe("Email must be in example.org or example.com, was: bar@example.net");
+  });
+
+  it("shows message with more array argument", () => {
+    expect(
+      localizedMessage(
+        {
+          code: "illegalEmailDomain",
+          email: "bar@example.net",
+          allowedDomains: ["example.org", "example.com", "foo.example.com"],
+        },
+        "en"
+      )
+    ).toBe(
+      "Email must be in example.org, example.com or foo.example.com, was: bar@example.net"
+    );
+  });
 });
